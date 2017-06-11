ac8245cd3f7fbce58dc7f0f7e7012d169f2f8671
Max Rasmusen <mrasmusen@spartaglobal.co>
2017-06-11 13:44:51 +0100

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,12 @@ b2 = Biscuit.create(:name => "Bourbon", :text => "Bourbons are also nice, especi
 
 Message.create(:user => 'chocolate-person', :text => 'I like chocolate so bourbons are nice', :biscuit_id => b2.id)
 Message.create(:user => 'also_likes-cohcoclate', :text => 'Yes they are', :biscuit_id => b2.id)
+
+b3 = Biscuit.create(:name => "Nice", :text => "Very nice biscuit", :num_ratings => 0, :rating_total => 0, :image_url => "http://i.imgur.com/7n0SkSk.jpg")
+
+Message.create(:user => 'Max', :text => 'Nice biscuits are tasty', :biscuit_id => b3.id)
+Message.create(:user => 'Alice', :text => 'Yes they are', :biscuit_id => b3.id)
+
+b4 = Biscuit.create(:name => "Digestive", :text => "Very common	 biscuit", :num_ratings => 0, :rating_total => 0, :image_url => "http://i.imgur.com/8D7Y3Ff.jpg")
+
+b5 = Biscuit.create(:name => "Hobnob", :text => "OK I guess", :num_ratings => 0, :rating_total => 0, :image_url => "http://i.imgur.com/lWAyJyB.jpg")
